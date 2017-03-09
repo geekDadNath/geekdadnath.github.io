fc25ace1b1780f940a732e2c3b5e20cef993c7d9
geekDadNath <Nath@piMac-Wired.lan>
2017-03-09 12:32:13 +0000

    NBB style

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,8 +1,9 @@
     var tree_structure = {
         chart: {
             container: "#geniebelt-portfolio",
-            levelSeparation:    70,
-            siblingSeparation:  32,
+            levelSeparation:    48,
+            siblingSeparation:  24,
+            subTeeSeparation:   32,
             nodeAlign: "TOP",
             animateOnInit: true,            
             node: {
@@ -28,44 +29,158 @@
             children: [
                 {
                     innerHTML: "#level2-1",
+                    collapsed: true,
                     children: [
                         {
                             innerHTML: "#level2-1-level3-1",
+                            collapsed: true,
+                            stackChildren: true,
                             children: [
                                 {
                                     innerHTML: "#level2-1-level3-1-level4-1"
+                                },
+                                {
+                                    innerHTML: "#level2-1-level3-1-level4-2"
+                                },
+                                {
+                                    innerHTML: "#level2-1-level3-1-level4-3"
+                                },
+                                {
+                                    innerHTML: "#level2-1-level3-1-level4-4"
+                                },
+                                {
+                                    innerHTML: "#level2-1-level3-1-level4-5"
+                                },
+                                {
+                                    innerHTML: "#level2-1-level3-1-level4-6"
                                 },
                             ]
                         },
                         {
                             innerHTML: "#level2-1-level3-2",
+                            collapsed: true,
+                            stackChildren: true,
                             children: [
                                 {
                                     innerHTML: "#level2-1-level3-2-level4-1"
                                 },
+                                {
+                                    innerHTML: "#level2-1-level3-2-level4-2"
+                                },
                             ]
+                        },
+                        {
+                            innerHTML: "#level2-1-level3-3",
+                            collapsed: true,
+                            stackChildren: true,
+                            children: [
+                                {
+                                    innerHTML: "#level2-1-level3-3-level4-1"
+                                },
+                                {
+                                    innerHTML: "#level2-1-level3-3-level4-2"
+                                },
+                            ]
+                        },
+                        {
+                            innerHTML: "#level2-1-level3-4"
                         }
                     ]
                 },
                 {
+                    childrenDropLevel: 1,
                     innerHTML: "#level2-2",
+                    collapsed: true,
                     children: [
                         {
-                            innerHTML: "#level2-2-level3-1"
+                            innerHTML: "#level2-2-level3-1",
+                            collapsed: true,
+                            stackChildren: true,
+                            children: [
+                                {
+                                    innerHTML: "#level2-2-level3-1-level4-1"
+                                },
+                                {
+                                    innerHTML: "#level2-2-level3-1-level4-2"
+                                },
+                                {
+                                    innerHTML: "#level2-2-level3-1-level4-3"
+                                },
+                            ]
                         },
                         {
                             innerHTML: "#level2-2-level3-2"
+                        },
+                        {
+                            innerHTML: "#level2-2-level3-3"
+                        },
+                        {
+                            innerHTML: "#level2-2-level3-4"
                         }
                     ]
                 },
                 {
                     innerHTML: "#level2-3",
+                    collapsed: true,
                     children: [
                         {
                             innerHTML: "#level2-3-level3-1"
                         },
                         {
                             innerHTML: "#level2-3-level3-2"
+                        },
+                        {
+                            innerHTML: "#level2-3-level3-3"
+                        },
+                        {
+                            innerHTML: "#level2-3-level3-4"
+                        },
+                        {
+                            innerHTML: "#level2-3-level3-5"
+                        }
+                    ]
+                },
+                {
+                    childrenDropLevel: 1,
+                    innerHTML: "#level2-4",
+                    collapsed: true,
+                    children: [
+                        {
+                            innerHTML: "#level2-4-level3-1"
+                        },
+                        {
+                            innerHTML: "#level2-4-level3-2"
+                        },
+                        {
+                            innerHTML: "#level2-4-level3-3"
+                        },
+                        {
+                            innerHTML: "#level2-4-level3-4"
+                        }
+                    ]
+                },
+                {
+                    innerHTML: "#level2-5",
+                    collapsed: true,
+                    children: [
+                        {
+                            innerHTML: "#level2-5-level3-1"
+                        },
+                        {
+                            innerHTML: "#level2-5-level3-2"
+                        }
+                    ]
+                },
+                {
+                    childrenDropLevel: 1,
+                    innerHTML: "#level2-6",
+                    collapsed: true,
+                    children: [
+                        {
+                            innerHTML: "#level2-6-level3-1"
+                        },
+                        {
+                            innerHTML: "#level2-6-level3-2"
                         }
                     ]
                 },
