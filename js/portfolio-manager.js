583eed2b49a0c5e0bfa3edfbe420cb4e517d28fe
geekDadNath <Nath@piMac-Wired.lan>
2017-03-16 14:35:52 +0000

    fake by-manager page

---
+++ js/portfolio-manager.js
@@ -0,0 +1,276 @@
+var tree_structure = {
+  chart: {
+    container: "#geniebelt-portfolio",
+    levelSeparation: 8,
+    siblingSeparation: 0,
+    subTeeSeparation: 136,
+    nodeAlign: "TOP",
+    animateOnInit: true,      
+    node: {
+      collapsable: true
+    },
+    connectors: {
+      type: "step",
+      style: {
+        "stroke-width": 2,
+        "stroke": "#dcdad2",
+        "arrow-end": "classic-wide"
+      }
+    },
+    animation: {
+      nodeAnimation: "easeOutBounce",
+      nodeSpeed: 700,
+      connectorsAnimation: "bounce",
+      connectorsSpeed: 700
+    }
+  },
+  nodeStructure: {
+    innerHTML: "#level1",
+    childrenDropLevel: 1,
+    children: [
+      {
+        pseudo: true,
+        children: [
+          {
+            innerHTML: "#level2-1",
+            collapsed: true,
+            stackChildren: true,
+            children: [
+              {
+                innerHTML: "#level2-1-level3-1"
+              },
+              {
+                innerHTML: "#level2-1-level3-2"
+              },
+              {
+                innerHTML: "#level2-1-level3-3"
+              },
+              {
+                innerHTML: "#level2-1-level3-4"
+              },
+              {
+                innerHTML: "#level2-1-level3-5"
+              },
+              {
+                innerHTML: "#level2-1-level3-6"
+              },
+              {
+                innerHTML: "#level2-1-level3-7"
+              },
+              {
+                innerHTML: "#level2-1-level3-8"
+              },
+              {
+                innerHTML: "#level2-1-level3-9"
+              },
+              {
+                innerHTML: "#level2-1-level3-10"
+              }
+            ]
+          },
+        ]
+      },
+      {
+        pseudo: true,
+        children: [
+          {
+            innerHTML: "#level2-2",
+            collapsed: true,
+            stackChildren: true,
+            children: [
+              {
+                innerHTML: "#level2-2-level3-1"
+              },
+              {
+                innerHTML: "#level2-2-level3-2"
+              },
+              {
+                innerHTML: "#level2-2-level3-3"
+              },
+              {
+                innerHTML: "#level2-2-level3-4"
+              },
+              {
+                innerHTML: "#level2-2-level3-5"
+              },
+              {
+                innerHTML: "#level2-2-level3-6"
+              },
+              {
+                innerHTML: "#level2-2-level3-7"
+              },
+              {
+                innerHTML: "#level2-2-level3-8"
+              },
+              {
+                innerHTML: "#level2-2-level3-9"
+              }
+            ]
+          },
+        ]
+      },
+      {
+        pseudo: true,
+        children: [
+          {
+            innerHTML: "#level2-3",
+            collapsed: true,
+            stackChildren: true,
+            children: [
+              {
+                innerHTML: "#level2-3-level3-1"
+              },
+              {
+                innerHTML: "#level2-3-level3-2"
+              },
+              {
+                innerHTML: "#level2-3-level3-3"
+              },
+              {
+                innerHTML: "#level2-3-level3-4"
+              },
+              {
+                innerHTML: "#level2-3-level3-5"
+              },
+              {
+                innerHTML: "#level2-3-level3-6"
+              },
+              {
+                innerHTML: "#level2-3-level3-7"
+              },
+              {
+                innerHTML: "#level2-3-level3-8"
+              },
+              {
+                innerHTML: "#level2-3-level3-9"
+              }
+            ]
+          },
+        ]
+      },
+      {
+        pseudo: true,
+        children: [
+          {
+            innerHTML: "#level2-4",
+            collapsed: true,
+            stackChildren: true,
+            children: [
+              {
+                innerHTML: "#level2-4-level3-1"
+              },
+              {
+                innerHTML: "#level2-4-level3-2"
+              },
+              {
+                innerHTML: "#level2-4-level3-3"
+              },
+              {
+                innerHTML: "#level2-4-level3-4"
+              },
+              {
+                innerHTML: "#level2-4-level3-5"
+              },
+              {
+                innerHTML: "#level2-4-level3-6"
+              },
+              {
+                innerHTML: "#level2-4-level3-7"
+              },
+              {
+                innerHTML: "#level2-4-level3-8"
+              },
+              {
+                innerHTML: "#level2-4-level3-9"
+              },
+              {
+                innerHTML: "#level2-4-level3-10"
+              },
+              {
+                innerHTML: "#level2-4-level3-11"
+              }
+            ]
+          },
+        ]
+      },
+      {
+        pseudo: true,
+        children: [
+          {
+            innerHTML: "#level2-5",
+            collapsed: true,
+            stackChildren: true,
+            children: [
+              {
+                innerHTML: "#level2-5-level3-1"
+              },
+              {
+                innerHTML: "#level2-5-level3-2"
+              },
+              {
+                innerHTML: "#level2-5-level3-3"
+              },
+              {
+                innerHTML: "#level2-5-level3-4"
+              },
+              {
+                innerHTML: "#level2-5-level3-5"
+              },
+              {
+                innerHTML: "#level2-5-level3-6"
+              },
+              {
+                innerHTML: "#level2-5-level3-7"
+              },
+              {
+                innerHTML: "#level2-5-level3-8"
+              },
+              {
+                innerHTML: "#level2-5-level3-9"
+              },
+              {
+                innerHTML: "#level2-5-level3-10"
+              }
+            ]
+          },
+        ]
+      },
+      {
+        pseudo: true,
+        children: [
+          {
+            innerHTML: "#level2-6",
+            collapsed: true,
+            stackChildren: true,
+            children: [
+              {
+                innerHTML: "#level2-6-level3-1"
+              },
+              {
+                innerHTML: "#level2-6-level3-2"
+              },
+              {
+                innerHTML: "#level2-6-level3-3"
+              },
+              {
+                innerHTML: "#level2-6-level3-4"
+              },
+              {
+                innerHTML: "#level2-6-level3-5"
+              },
+              {
+                innerHTML: "#level2-6-level3-6"
+              },
+              {
+                innerHTML: "#level2-6-level3-7"
+              },
+              {
+                innerHTML: "#level2-6-level3-8"
+              }
+            ]
+          },
+        ]
+      },
+    ]
+  }
+};
